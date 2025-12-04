7902c478ddf4b2f351f51e7de7a780c8333fd011
Lucia Paulino <lcpaulin@usc.edu>
2025-12-03 21:07:49 -0800

    attempting two rec functions separate

--- wordle.cpp
+++ wordle.cpp
@@ -14,6 +14,8 @@ using namespace std;
 
 // Add prototypes of helper functions here
 void wordle_helper(std::set<std::string>& out, std::string in, int pos); 
+void wordle_floats(vector<string> part, vector<char> floats, std::string in, int pos); 
+void wordle_rest(std::set<std::string>& out, string in, int pos); 
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,35 +24,70 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
 
-    std::set<std::string> temp_words;
-    wordle_helper(temp_words,in, 0); 
+        vector<char> floaters; 
+        for (auto c : floating) {
+            floaters.push_back(c); 
+        }
+        vector<string> partial; 
+        wordle_floats(partial, floaters, in, 0); 
+        std::set<std::string> out;
+        for (int i = 0; i < floaters.size(); i++)
+        {wordle_rest(out, partial[i],0); }
+    
+        for (auto word : out){
+            std::cout << word << endl;
+        }
 
-    return temp_words; 
+        return out; 
 }
 
 // Define any helper functions here
-void wordle_helper(std::set<std::string>& out, std::string in, int pos){
+void wordle_floats(vector<string> part, vector<char> floats, std::string in, int pos){
 
     if (pos == in.size() ){
+        part.push_back(in); 
+        return; 
+    } 
+
+    if (in[pos] != '-'){
+        wordle_floats(part, floats, in, pos+1); 
+        // fixed , keep going 
+    }
+
+    // now generate all combos with these two letters floating around 
+    for (int i = 0; i < floats.size(); i++){
+        // explore 
+        in[pos] = floats[i]; 
+        wordle_floats(part, floats, in, pos+1); 
+    }
+    in[pos] = '-'; // undo! 
+
+} 
+
+
+// Define any helper functions here
+void wordle_rest(std::set<std::string>& out, string in, int pos){
+
+    if (pos == in.size()){
         out.insert(in); 
         return; 
     }
 
-    for (int i = 0; i <= 25; i++){
-        // choose 
-        if (in[pos] == '-'){
-            wordle_helper(out,in, pos+1); 
-            // fixed , keep going 
-        }
+      // choose 
+      if (in[pos] != '-'){
+        wordle_rest(out,in, pos+1); 
+        // set , keep going 
+    }
 
+    for (int i = 0; i <= 25; i++){
         // explore 
         in[pos] = 'a' + i; 
-        wordle_helper(out,in, pos+1); 
-
-        in[pos] = '-'; // undo! 
+        wordle_rest(out,in, pos+1); 
 
     }
+    in[pos] = '-'; // undo! 
     // undo ! 
 
 }
+
 
