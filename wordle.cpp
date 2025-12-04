9e68ac3703b8e72656445c06c2f801d08c0d55b8
Lucia Paulino <lcpaulin@usc.edu>
2025-12-03 17:34:08 -0800

    adding to codio to check first

--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordle_helper(std::set<std::string>& out, std::string in, int pos); 
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -21,8 +21,30 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
 
+    std::set<std::string> temp_words;
+    wordle_helper(temp_words,"-i--", 0); 
+
+    return temp_words; 
 }
 
 // Define any helper functions here
+void wordle_helper(std::set<std::string>& out, std::string in, int pos){
+
+    if (pos == in.size() ){
+        out.insert(in); 
+    }
+
+    for (int i = 0; i <= 25; i++){
+        if (in[pos] == '-')
+        {
+        in[pos] = 'a' + 1; 
+        wordle_helper(out,in, pos+1); 
+        in[pos] = '-'; 
+    
+        }
+
+    }
+    // undo ! 
+
+}
