6508920ed00fc0a2e7539bf975e0040dc487680d
Lucia Paulino <lcpaulin@usc.edu>
2025-12-03 22:58:58 -0800

    rec check

--- wordle.cpp
+++ wordle.cpp
@@ -25,53 +25,15 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
 
-    std::vector<char> floaters; 
-        for (auto c : floating) {
-            floaters.push_back(c); 
-        }
-        std::vector<string> partial; 
-        wordle_floats(partial, floaters, in, 0); 
-        std::set<std::string> out;
-        for (int i = 0; i < floaters.size(); i++)
-        {wordle_rest(out, partial[i],0); }
-    
-        for (auto word : out){
-            std::cout << word << endl;
-        }
 
-        return out; 
+
+    
 }
 
 // Define any helper functions here
-void wordle_floats(std::vector<string> part, std::vector<char> floats, std::string in, int pos){
-
-    if (pos == in.size() ){
-        std::cout << in << endl; 
-        part.push_back(in); 
-        return; 
-    } 
-
-    if (in[pos] != '-'){
-        wordle_floats(part, floats, in, pos+1); 
-        // fixed , keep going 
-    }
-
-    // now generate all combos with these two letters floating around 
-    for (int i = 0; i < floats.size(); i++){
-        // explore 
-        in[pos] = floats[i]; 
-        wordle_floats(part, floats, in, pos+1); 
-    }
-    in[pos] = '-'; // undo! 
-
-} 
-
-
-// Define any helper functions here
-void wordle_rest(std::set<std::string>& out, string in, int pos){
+void worlde_helper(std::vector<string> part, std::vector<char> floats, std::string in, int pos){
 
     if (pos == in.size()){
-        
         out.insert(in); 
         return; 
     }
@@ -90,7 +52,14 @@ void wordle_rest(std::set<std::string>& out, string in, int pos){
     }
     in[pos] = '-'; // undo! 
     // undo ! 
+    
+} 
 
+
+// Define any helper functions here
+void wordle_rest(std::set<std::string>& out, string in, int pos){
+
+    
 }
 
 
