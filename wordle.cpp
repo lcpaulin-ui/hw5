cb298ea438d1a0b2c8f5c8e7ffada2868c8bf4b0
Lucia Paulino <lcpaulin@usc.edu>
2025-12-03 23:35:40 -0800

    xchecking dict

--- wordle.cpp
+++ wordle.cpp
@@ -14,7 +14,7 @@ using namespace std;
 
 // Add prototypes of helper functions here
 void wordle_helper(std::set<std::string>& out, std::string in, int pos); 
-void wordle_floaters(std::set<std::string>& out, std::string floaters, std::string in, int floater_cnt,  int pos); 
+void wordle_floaters(std::set<std::string>& out, std::string floaters, std::string in, int floater_cnt,  int pos, const std::set<std::string>& dict); 
 void wordle_remaining(std::set<std::string>& out, std::string in, int pos, const std::set<std::string>& dict); 
 
 // Definition of primary wordle function
@@ -25,23 +25,23 @@ std::set<std::string> wordle(
 {
 
     std::set<std::string> temp_words;
-    wordle_floaters(temp_words, floating, in, 0, 0);
+    wordle_floaters(temp_words, floating, in, 0, 0, dict);
     return temp_words; 
 }
 
 
 // Define any helper functions here
-void wordle_floaters(std::set<std::string>& out, std::string floaters, std::string in, int floater_cnt,  int pos){
+void wordle_floaters(std::set<std::string>& out, std::string floaters, std::string in, int floater_cnt,  int pos, const std::set<std::string>& dict){ 
 
     // base case: placed all floating letters 
     if (floater_cnt == floaters.size()){
-        wordle_remaining(out, in, 0, const std::set<std::string>& dict); 
+        wordle_remaining(out, in, 0, dict); 
     }
             for (int i = 0; i < in.size(); i++){
                 // explore 
                 if (in[i] == '-'){
                     in[i] = floaters[floater_cnt];
-                    wordle_floaters(out, floaters, in, floater_cnt+1, pos+1); 
+                    wordle_floaters(out, floaters, in, floater_cnt+1, pos+1, dict); 
                     in[i] = '-'; 
                 }
     
@@ -54,14 +54,15 @@ void wordle_floaters(std::set<std::string>& out, std::string floaters, std::stri
 // Define any helper functions here
 void wordle_remaining(std::set<std::string>& out, std::string in, int pos, const std::set<std::string>& dict){
     if (pos == in.size()){
+    
         if (dict.find(in) != dict.end())
-       { out.insert(in); }
+      { out.insert(in); }
         return; 
     }
 
       // choose 
     if (in[pos] != '-'){
-        wordle_remaining(out, in, pos+1); 
+        wordle_remaining(out, in, pos+1, dict); 
         // set , keep going 
     }
     else 
@@ -69,7 +70,7 @@ void wordle_remaining(std::set<std::string>& out, std::string in, int pos, const
         for (int i = 0; i <= 25; i++){
             // explore 
             in[pos] = 'a'+ i; 
-            wordle_remaining(out, in, pos+1); 
+            wordle_remaining(out, in, pos+1, dict); 
 
         }
         in[pos] = '-'; // undo! 
